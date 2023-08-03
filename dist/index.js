a07e4a2262a02b7e49b701aff5a2995b07833745
Y0ooSibbi <alpesh57678@gmail.com>
2023-08-03 18:12:29 +0530

    npx tsc command added

--- dist/index.js
+++ dist/index.js
@@ -15,86 +15,81 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const promise_1 = require("mysql2/promise");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000; // Change this to the desired port
+const port = process.env.PORT || 3000; // Change this to the desired port
 const pool = (0, promise_1.createPool)({
-    host: 'sql6.freesqldatabase.com',
-    user: 'sql6637212',
-    password: '5QR7s3hIdc',
-    database: 'sql6637212',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     connectionLimit: 10,
 });
 app.use(body_parser_1.default.json());
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const connection = yield pool.getConnection();
-        console.log('Connected to the database!');
-        const email = 'test@example.com'; // Replace with your test email
-        const phoneNumber = '1234567890';
-        // Perform a test query to check the database connection
-        const emailParam = email || null;
-        const phoneNumberParam = phoneNumber || null;
-        const [rows] = yield connection.execute('SELECT * FROM Contact WHERE email = ? OR phoneNumber = ?', [emailParam, phoneNumberParam]);
-        // Use type assertion to properly cast the results to ContactRow[]
-        const contacts = rows;
-        console.log('Test query result:', contacts);
-        connection.release();
-    }
-    catch (error) {
-        console.error('Error connecting to the database:');
-        return;
-    }
-}))();
-// (async () => {
-//     try {
-//       const connection = await pool.getConnection();
-//       console.log('Connected to the database!');
-//       connection.release();
-//     } catch (error) {
-//       console.error('Error connecting to the database:');
-//     }
-//   })();
 app.post('/identify', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, phoneNumber } = req.body;
+    const { email, phoneNumber } = req.body || null;
     try {
         const connection = yield pool.getConnection();
+        console.log(email);
         // Find the contact with the same email or phoneNumber
-        const [rows] = yield connection.execute('SELECT * FROM Contact WHERE email = ? OR phoneNumber = ?', [email, phoneNumber]);
+        const [rows] = yield connection.execute('SELECT * FROM Contact WHERE email = ? OR phoneNumber = ?', [email === undefined ? null : email, phoneNumber === undefined ? null : phoneNumber]);
         const contacts = rows;
-        console.log('Test query result:', contacts);
-        console.log('Test query result:', rows);
         if (contacts.length > 0) {
             const existingContact = contacts[0];
-            // If contact exists, create a secondary contact if there's new information
-            const newContact = {
-                id: contacts.length + 1,
-                phoneNumber: phoneNumber || existingContact.phoneNumber,
-                email: email || existingContact.email,
-                linkedId: existingContact.linkPrecedence === 'primary' ? existingContact.id : existingContact.linkedId,
-                linkPrecedence: 'secondary',
-                createdAt: new Date().toISOString(),
-                updatedAt: new Date().toISOString(),
-                deletedAt: null,
-            };
-            // Save the new contact to the database
-            const [insertResult] = yield connection.query('INSERT INTO Contact SET ?', newContact);
-            //   newContact.id = insertResult.insertId;
-            connection.release();
-            //   res.status(200).json({
-            //     contact: {
-            //       primaryContatctId: existingContact.linkPrecedence === 'primary' ? existingContact.id : existingContact.linkedId,
-            //       emails: [existingContact.email, newContact.email].filter((e) => e !== null),
-            //       phoneNumbers: [existingContact.phoneNumber, newContact.phoneNumber].filter((p) => p !== null),
-            //       secondaryContactIds: rows
-            //         .filter((contact) => contact.linkedId === existingContact.id)
-            //         .map((contact) => contact.id),
-            //     },
-            //   });
+            // Update the second primary contact to be a secondary contact
+            if (contacts.length > 1) {
+                const secondPrimaryContact = contacts[1];
+                // Updates the contact only when it's primary other-wise no need to update 
+                if (contacts[1].linkPrecedence === 'primary') {
+                    yield connection.query('UPDATE Contact SET linkPrecedence = ?,linkedId = ?, updatedAt = ? WHERE id = ?', ['secondary', contacts[0].id, new Date().toISOString(), contacts[1].id]);
+                }
+                // Remove duplicates from emails and phoneNumbers arrays
+                const emailsSet = new Set([existingContact.email, secondPrimaryContact.email].filter((e) => e !== null));
+                const phoneNumbersSet = new Set([existingContact.phoneNumber, secondPrimaryContact.phoneNumber].filter((p) => p !== null));
+                res.status(200).json({
+                    contact: {
+                        primaryContatctId: existingContact.linkPrecedence === 'primary' ? existingContact.id : existingContact.linkedId,
+                        emails: Array.from(emailsSet),
+                        phoneNumbers: Array.from(phoneNumbersSet),
+                        secondaryContactIds: contacts
+                            .filter((contact) => contact.linkedId === existingContact.id)
+                            .map((contacts) => contacts.id),
+                    }
+                });
+            }
+            else {
+                // If contact exists, create a secondary contact if there's new information
+                const newContact = {
+                    phoneNumber: phoneNumber || existingContact.phoneNumber,
+                    email: email || existingContact.email,
+                    linkedId: existingContact.linkPrecedence === 'primary' ? existingContact.id : existingContact.linkedId,
+                    linkPrecedence: 'secondary',
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString(),
+                    deletedAt: null,
+                };
+                // Save the new contact to the database
+                const [insertResult] = yield connection.query('INSERT INTO Contact SET ?', newContact);
+                // Remove duplicates from emails and phoneNumbers arrays
+                const emailsSet = new Set([existingContact.email, newContact.email].filter((e) => e !== null));
+                const phoneNumbersSet = new Set([existingContact.phoneNumber, newContact.phoneNumber].filter((p) => p !== null));
+                res.status(200).json({
+                    contact: {
+                        primaryContatctId: existingContact.linkPrecedence === 'primary' ? existingContact.id : existingContact.linkedId,
+                        emails: Array.from(emailsSet),
+                        phoneNumbers: Array.from(phoneNumbersSet),
+                        secondaryContactIds: contacts
+                            .filter((contact) => contact.linkedId === existingContact.id)
+                            .map((contacts) => contacts.id),
+                    },
+                });
+                connection.release();
+            }
         }
         else {
             // If contact doesn't exist, create a new primary contact
             const newContact = {
-                id: contacts.length + 1,
                 phoneNumber,
                 email,
                 linkedId: null,
